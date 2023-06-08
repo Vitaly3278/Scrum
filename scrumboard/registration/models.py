a4a7e3aecf0fa1cebb79f295f943f096132bc5bb
Vitaly <vital-nvl@mail.ru>
2023-06-08 17:25:32 +0300

    create class Users

--- scrumboard/registration/models.py
+++ scrumboard/registration/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    e_mail = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    time_create = models.DateTimeField(auto_now_add=True)
